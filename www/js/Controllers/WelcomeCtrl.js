53f0a32531ae48ea2a928b15868759841de38a87
Jimmy Tsao <jimmy.tsao@gmail.com>
2015-11-15 11:17:09 -0600

    add homepage image

--- www/js/Controllers/WelcomeCtrl.js
+++ www/js/Controllers/WelcomeCtrl.js
@@ -5,17 +5,24 @@ angular.module('starter.controllers')
   //   $state.go('home');
   // }, 2000);
   //
+  var times = 3000;
   $scope.currentIndex = {
     index: 0
   };
 
-  $timeout(updateIndex, 2000);
+  $timeout(updateIndex, times);
 
   function updateIndex(){
+
+    console.log('scope', $scope.currentIndex);
     $scope.currentIndex.index++;
 
-    if ($scope.currentIndex < 3){
-      updateIndex();
+    if ($scope.currentIndex.index < 4){
+      $timeout(updateIndex, times);
+    }
+
+    if ($scope.currentIndex.index === 3){
+      $state.go('home');
     }
   }
 });
