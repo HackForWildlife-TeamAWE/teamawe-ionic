a5f9ad83fd984fab60e436efbc0808c36d0530c5
Jimmy Tsao <jimmy.tsao@gmail.com>
2015-11-15 13:05:30 -0600

    minor adjustments

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,18 +1,3 @@
-var map;
-window.initMap = function initMap() {
-  if(document.getElementById('google-map')){
-    map = new google.maps.Map(document.getElementById('google-map'), {
-      center: {lat: 39.0997, lng: -94.5783},
-      zoom: 12,
-      scrollwheel:false,
-      mapTypeId: google.maps.MapTypeId.SATELLITE
-    });
-
-    //var styles = [{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45},{"visibility":"simplified"}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"on"},{"hue":"#ff0000"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"visibility":"off"}]},{"featureType":"road.highway","elementType":"labels.icon","stylers":[{"visibility":"on"},{"weight":"1.89"}]},{"featureType":"road.arterial","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"transit.line","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"transit.line","elementType":"geometry.fill","stylers":[{"color":"#469ee0"}]},{"featureType":"transit.station.bus","elementType":"all","stylers":[{"visibility":"on"},{"hue":"#ff0000"}]},{"featureType":"transit.station.bus","elementType":"geometry","stylers":[{"visibility":"off"}]},{"featureType":"transit.station.bus","elementType":"labels","stylers":[{"visibility":"on"}]},{"featureType":"transit.station.rail","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"transit.station.rail","elementType":"geometry","stylers":[{"visibility":"on"}]},{"featureType":"transit.station.rail","elementType":"labels","stylers":[{"visibility":"on"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#469ee0"},{"visibility":"on"}]}]
-    //map.setOptions({styles: styles});
-
-  }
-}
 angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function($scope) {})
@@ -42,7 +27,7 @@ angular.module('starter.controllers', [])
   };
 })
 .controller('MapCtrl', function($scope, $http, $timeout) {
-  $scope.map = { center: { latitude: 0.1930, longitude: 37.44 }, zoom: 15 };
+  $scope.map = { center: { latitude: 0.1930, longitude: 37.44 }, zoom: 14 };
   $scope.markers = [];
   $scope.date = new Date(2015,0,15);
 
