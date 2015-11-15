b1e4783159c345ff6bf6ca6cf418cb1f1b31d603
Jimmy Tsao <jimmy.tsao@gmail.com>
2015-11-15 10:52:35 -0600

    add fonts and welcome

--- www/js/Controllers/WelcomeCtrl.js
+++ www/js/Controllers/WelcomeCtrl.js
@@ -1,7 +1,21 @@
 angular.module('starter.controllers')
 
 .controller('WelcomeCtrl', function($scope, $timeout, $state) {
-  $timeout(function(){
-    $state.go('home');
-  }, 2000);
+  // $timeout(function(){
+  //   $state.go('home');
+  // }, 2000);
+  //
+  $scope.currentIndex = {
+    index: 0
+  };
+
+  $timeout(updateIndex, 2000);
+
+  function updateIndex(){
+    $scope.currentIndex.index++;
+
+    if ($scope.currentIndex < 3){
+      updateIndex();
+    }
+  }
 });
